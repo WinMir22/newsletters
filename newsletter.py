8779e596c4617eb37503d9be8a29a7b25f5b4d80
Мирон <miron.kiselev111@gmail.com>
2025-11-21 12:17:48 +0300

    Refactor Newsletter model by updating fields and imports
    Removed unused columns and updated imports.

--- newsletter.py
+++ newsletter.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, text as text_f
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy import DateTime, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.database import Base
-from app.enums import NewsletterStatus
+from app.infrastructure.database import Base
+from app.infrastructure.enums import NewsletterStatus
 
 
 class Newsletter(Base):
@@ -14,14 +13,5 @@ class Newsletter(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     status: Mapped[str] = mapped_column(server_default=NewsletterStatus.WAIT)
     text: Mapped[str] = mapped_column(nullable=False)
-    offset: Mapped[int] = mapped_column(server_default="0")
-    start_datetime: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=text_f("TIMEZONE('utc', NOW())"),
-        nullable=False,
-    )
-    buttons: Mapped[dict[int, list[str]]] = mapped_column(JSON, nullable=True)
-    is_game_notification: Mapped[bool] = mapped_column(
-        nullable=False, server_default="False"
-    )
-    photo: Mapped[str] = mapped_column(nullable=True)
+    owner_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    start_datetime: Mapped[datetime] = mapped_column(DateTime(timezone=True))
